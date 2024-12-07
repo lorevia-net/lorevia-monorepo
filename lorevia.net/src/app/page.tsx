22601319d28be203f64331c87b063e8a3c88fefe
Christian Camacho <chris.ca.dev@gmail.com>
2024-12-07 12:53:29 -0800

    add the started hybrid editor

--- lorevia.net/src/app/page.tsx
+++ lorevia.net/src/app/page.tsx
@@ -1,16 +1,6 @@
 'use client';
-
-import React from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import { markdown } from '@codemirror/lang-markdown';
-
 function HybridEditor() {
-    const [value, setValue] = React.useState("console.log('hello world!');");
-    const onChange = React.useCallback((val: string, viewUpdate: any) => {
-        console.log('val:', val);
-        setValue(val);
-    }, []);
-    return <div contentEditable={true}>Hola</div>;
+    return <div contentEditable onInput={(e) => console.log({ text: e.currentTarget.textContent })}>Hola</div>;
 }
 
 export default HybridEditor;
